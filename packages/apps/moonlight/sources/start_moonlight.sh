40ef158e0f3555746a1dd2025b5bea8896f79634
John Williams <porschemad911@gmail.com>
2025-01-29 21:46:18 +1100

    Moonlight / Portmaster - refactor controller layout logic

--- packages/apps/moonlight/sources/start_moonlight.sh
+++ packages/apps/moonlight/sources/start_moonlight.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 . /etc/profile
+
+# Controller layout
+GAME=$(echo "${1}"| sed "s#^/.*/##")
+PLATFORM=$(echo "${2}"| sed "s#^/.*/##")
+/usr/bin/controller-layout "${PLATFORM}" "${GAME}" "/storage/.config/moonlight/gamecontrollerdb.txt"
+
+# Launch moonlight
 set_kill set "moonlight"
 QT_QPA_PLATFORM=wayland moonlight
